a9b896b697abd232271fb7a9b66ba54dc1a20f58
Daniel Españadero <developer@daniel-espanadero.com>
2022-08-13 19:42:41 +0200

    App finished

--- src/clases/ClasePadre_Abstracta.java
+++ src/clases/ClasePadre_Abstracta.java
@@ -14,19 +14,19 @@ public abstract class ClasePadre_Abstracta {
 
 		do {
 			do {
-				System.out.print("Por favor, seleccione una opcion:");
-				System.out.print("	1. Consulta de saldo.");
-				System.out.print("	2. Retiro de efectivo.");
-				System.out.print("	3. Deposito de efectivo.");
-				System.out.print("	4. Salir.");
+				System.out.println("Por favor, seleccione una opcion:");
+				System.out.println("	1. Consulta de saldo.");
+				System.out.println("	2. Retiro de efectivo.");
+				System.out.println("	3. Deposito de efectivo.");
+				System.out.println("	4. Salir.");
 				seleccion = entrada.nextInt();
 
 				if (seleccion >= 1 && seleccion <= 4) {
 					bandera = 1;
 				} else {
-					System.out.print("----------------------------------------------------");
-					System.out.print("Opcion no disponible, vuelva a intentarlo por favor.");
-					System.out.print("----------------------------------------------------");
+					System.out.println("----------------------------------------------------");
+					System.out.println("Opcion no disponible, vuelva a intentarlo por favor.");
+					System.out.println("----------------------------------------------------");
 				}
 			} while (bandera == 0);
 
@@ -43,6 +43,7 @@ public abstract class ClasePadre_Abstracta {
 				System.out.println("-----------------------");
 				System.out.println("Gracias, vuelva pronto!");
 				System.out.println("-----------------------");
+				bandera = 2;
 
 			}
 		} while (bandera != 2);
